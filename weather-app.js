ab2a4dbbdacb81082b00c35d7f8317407e787d4f
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2020-09-14 17:47:46 -0700

    finished display functionality

--- weather-app.js
+++ weather-app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
-const display = document.querySelector('.display')
-const details = document.querySelector('.details')
+const display = document.querySelector('.display');
+const details = document.querySelector('.details');
+const image = document.querySelector('.display img');
+const icon = document.querySelector('.icon img');
 
 const updateDisplay = data => {
   const cityDetails = data.cityDetails;
@@ -15,16 +17,33 @@ const updateDisplay = data => {
     </div>
   `;
 
+  let imageSource = null;
+  if(weather.IsDayTime) {
+    imageSource = 'images/day.svg';
+  } else {
+    imageSource = 'images/night.svg';
+  }
+  image.setAttribute('src', imageSource);
+
+  const iconSource = `images/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSource);
+
+
+
   if (display.classList.contains('hidden')) {
     display.classList.remove('hidden');
   }
+
+  
+
+
 
 }
 
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
   const weather = await getWeather(cityDetails.Key);
-
+  console.log(weather);
   return {
     cityDetails: cityDetails,
     weather: weather
